ee1f966b95c0a0e6c280e04e290d11cef3b2e4df
shzkhan1111 <91877371+shzkhan1111@users.noreply.github.com>
2021-11-09 11:39:34 +0500

    Add project files.

---
+++ Libraries/Nop.Core/Domain/CustomersTier/CustomerTier.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nop.Core.Domain.CustomersTier
+{
+    class CustomerTier : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
